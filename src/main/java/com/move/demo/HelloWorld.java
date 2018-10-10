37ecdb2ecba01f82fd0d8d3d4047eb929782f5f7
wxj <1213510363@qq.com>
2018-10-10 10:07:51 +0800

    wxj

--- src/main/java/com/move/demo/HelloWorld.java
+++ src/main/java/com/move/demo/HelloWorld.java
@@ -5,10 +5,12 @@ import com.move.model.GirlInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping(value = "/hello")
 public class HelloWorld {
 
 
@@ -16,9 +18,9 @@ public class HelloWorld {
     public GirlInfo girl;
 
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String say(){
-        return  girl.getCupSize();
+    @RequestMapping(value = "/say" , method = RequestMethod.GET)
+    public String say(@RequestParam(value = "id",required = false,defaultValue = "0") Integer id){
+        return  "id:"+id;
     }
 
 }
